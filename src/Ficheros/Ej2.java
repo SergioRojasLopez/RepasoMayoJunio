ca81babe67a504989999e713e39a850dcdb376ef
Sergio Rojas <sergiorojaslopez12@gmail.com>
2024-06-13 14:47:04 +0200

    Ficheros ejercicios - in process

--- src/Ficheros/Ej2.java
+++ src/Ficheros/Ej2.java
@@ -12,13 +12,13 @@ public class Ej2 {
     public static void main(String[] args) {
 
         Path fichero = Paths.get("C:\\Users\\Alumno\\IdeaProjects\\RepasoMayoJunio\\src\\Ficheros\\Ej2.txt");
-        Pattern patron = Pattern.compile("(\\d{4}[B-Z&&[^EIOU]]{3})");
-        Pattern patronDeFin = Pattern.compile("[;:]$");
+        Pattern matriculaValida = Pattern.compile("(\\d{4}[B-Z&&[^EIOU]]{3})", Pattern.CASE_INSENSITIVE);
+        Pattern lineaValida = Pattern.compile(".*[;:]$");
 
         try (Stream<String> flujo = Files.lines(fichero)){
-            flujo.map(patronDeFin::matcher)
+            flujo.map(lineaValida::matcher)
                     .filter(Matcher::matches)
-                    .map(m->patron.matcher(m.group(0)))
+                    .map(m->matriculaValida.matcher(m.group(0)))
                     .forEach(m->{
                         while (m.find()){
                             System.out.println(m.group(0) + " ");
@@ -29,5 +29,32 @@ public class Ej2 {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public static void main2(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Alumno\\IdeaProjects\\RepasoMayoJunio\\src\\Ficheros\\Ej2.txt"))){
+            String linea;
+            Pattern lineaValida = Pattern.compile(".*[;:]$");
+            Pattern matriculaValida = Pattern.compile("(\\d{4}[B-Z&&[^EIOU]]{3})", Pattern.CASE_INSENSITIVE);
+
+            while ((linea = br.readLine()) != null) {
+                Matcher mLineaValida = lineaValida.matcher(linea);
+
+                if (mLineaValida.matches()){
+                    Matcher mMatriculaValida = matriculaValida.matcher(linea);
+
+                    while (mMatriculaValida.find()){
+                        System.out.print(mMatriculaValida.group() + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        //lecturas de ficheros
+        //recorridos directorios
+        //Expresiones regulares
+        //Creacion de ficheros
     }
 }
